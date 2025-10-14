e766b551459da1caa60c8c380e5ad5217a133119
Marco Rafael Simões Costa <markcostah@gmail.com>
2025-10-14 20:11:49 +0100

    Simplify project

--- backend/ml/scripts/main.py
+++
@@ -1,7 +0,0 @@
-from post_processor import PostProcessor
-
-processor = PostProcessor()
-
-if __name__ == "__main__":
-  plate = processor.validate("A120CD")
-  print(plate)
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from post_processor import PostProcessor
+from extract import Extractor
+from detect import Detector
+
+extractor = Extractor()
+detector = Detector()
+processor = PostProcessor()
+
+# Evaluate the performance of model flow (extractor & detector) to performance of the processor.
+# Test with 100 images to calculate the accuracy of detector and accuracy of the processor.
+
+# To evaluate the extractor also measure the confidence so we can see how the confidence correlates with accuracy.
+
+if __name__ == "__main__":
+  pass
